972df14f529d0d6545a07a504629c41e074efb38
David Mendoza <david.mendoza.mnez@gmail.com>
2019-03-24 09:34:47 +0100

    Add row count in logging and simplify string formats

--- geocoder.py
+++ geocoder.py
@@ -16,41 +16,45 @@ try:
 
     with open(path, newline='', encoding='utf-8') as input_file:
         reader = csv.reader(input_file)
-
-        with open('output_{name}'.format(name=os.path.basename(input_file.name)), 'w', newline='', encoding='utf-8') \
+        rows = list(reader)
+        row_count = len(rows)
+        with open(f"output_{os.path.basename(input_file.name)}", 'w', newline='', encoding='utf-8') \
                 as output_file:
             writer = csv.writer(output_file)
             writer.writerow(["Address", "Latitude", "Longitude", "Formatted Address"])
-            count = 0
+            geocoded = 0
 
-            for row in reader:
-                logging.info(','.join(row))
-                logging.info((API_ENDPOINT.format(key=key, address=quote('+'.join(row)))))
+            for i, row in enumerate(rows):
                 if len(row):
-                    response = urlopen(API_ENDPOINT.format(key=key, address=quote('+'.join(row))))
+                    logging.info(f"{i+1}/{row_count}: {','.join(row)}")
+
+                    url = API_ENDPOINT.format(key=key, address=quote('+'.join(row)))
+                    logging.info(url)
+
+                    response = urlopen(url)
                     data = json.loads(response.read())
 
                     if data.get('results'):
                         result = data['results'].pop(0)
-                        writer.writerow([','.join(row), format(result['geometry']['location']['lat'], '.7f'),
+                        writer.writerow([','.join(row),
+                                         format(result['geometry']['location']['lat'], '.7f'),
                                          format(result['geometry']['location']['lng'], '.7f'),
                                          result['formatted_address']])
-                        count += 1
+                        geocoded += 1
                     elif 'error_message' in data:
-                        logging.error('Google Maps Geocoding - {message}'.format(message=data['error_message']))
+                        logging.error(f"Google Maps Geocoding - {data['error_message']}")
                         exit(1)
                     else:
                         writer.writerow([','.join(row), 'No results', 'No results', 'No results'])
                 else:
                     writer.writerow(['', 'No results', 'No results', 'No results'])
 
-            logging.info('{count} addresses geocoded successfully.'.format(count=count))
+            logging.info(f"{geocoded} addresses geocoded successfully.")
 
 except FileNotFoundError:
     logging.error('CSV file not found.')
     exit(1)
 except HTTPError as error:
     error_message = json.loads(error.read())['error_message']
-    logging.error('Google Maps Geocoding [{code}: {reason}] - {message}'.format(code=error.code, reason=error.reason,
-                                                                                message=error_message))
+    logging.error(f"Google Maps Geocoding [{error.code}: {error.reason}] - {error_message}")
     exit(1)
