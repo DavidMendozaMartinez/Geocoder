8cd6ef69d06c7889db049404f037d30df8a9c7d2
David Mendoza <david.mendoza.mnez@gmail.com>
2019-03-24 07:51:32 +0100

    Fix encoding bug, add Formatted Address column and logging lines

--- geocoder.py
+++ geocoder.py
@@ -14,32 +14,35 @@ try:
     path = sys.argv[1] if len(sys.argv) > 1 else input('Enter the path of the csv file: ')
     key = os.getenv('GOOGLE_MAPS_GEOCODING_API_KEY') or input('Enter the Google Maps Geocoding API KEY: ')
 
-    with open(path, newline='') as input_file:
+    with open(path, newline='', encoding='utf-8') as input_file:
         reader = csv.reader(input_file)
 
-        with open('output_{name}'.format(name=os.path.basename(input_file.name)), 'w', newline='') as output_file:
+        with open('output_{name}'.format(name=os.path.basename(input_file.name)), 'w', newline='', encoding='utf-8') \
+                as output_file:
             writer = csv.writer(output_file)
-            writer.writerow(["Address", "Latitude", "Longitude"])
+            writer.writerow(["Address", "Latitude", "Longitude", "Formatted Address"])
             count = 0
 
             for row in reader:
+                logging.info(','.join(row))
+                logging.info((API_ENDPOINT.format(key=key, address=quote('+'.join(row)))))
                 if len(row):
                     response = urlopen(API_ENDPOINT.format(key=key, address=quote('+'.join(row))))
-                    encoding = response.info().get_content_charset('utf-8')
-                    data = json.loads(response.read().decode(encoding))
+                    data = json.loads(response.read())
 
                     if data.get('results'):
                         result = data['results'].pop(0)
-                        writer.writerow([''.join(row), format(result['geometry']['location']['lat'], '.7f'),
-                                         format(result['geometry']['location']['lng'], '.7f')])
+                        writer.writerow([','.join(row), format(result['geometry']['location']['lat'], '.7f'),
+                                         format(result['geometry']['location']['lng'], '.7f'),
+                                         result['formatted_address']])
                         count += 1
                     elif 'error_message' in data:
                         logging.error('Google Maps Geocoding - {message}'.format(message=data['error_message']))
                         exit(1)
                     else:
-                        writer.writerow([''.join(row), 'No results', 'No results'])
+                        writer.writerow([','.join(row), 'No results', 'No results', 'No results'])
                 else:
-                    writer.writerow(['', 'No results', 'No results'])
+                    writer.writerow(['', 'No results', 'No results', 'No results'])
 
             logging.info('{count} addresses geocoded successfully.'.format(count=count))
 
@@ -47,8 +50,7 @@ except FileNotFoundError:
     logging.error('CSV file not found.')
     exit(1)
 except HTTPError as error:
-    encoding = error.info().get_content_charset('utf-8')
-    error_message = json.loads(error.read().decode(encoding))['error_message']
+    error_message = json.loads(error.read())['error_message']
     logging.error('Google Maps Geocoding [{code}: {reason}] - {message}'.format(code=error.code, reason=error.reason,
                                                                                 message=error_message))
     exit(1)
